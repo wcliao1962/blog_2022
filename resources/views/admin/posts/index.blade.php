7dbc0ac0e396b1f10279c9f04954e5e8f91a6558
M514-T\admin <@>
2022-11-16 11:02:54 +0800

    將文章資料顯示在後台的表格裡

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -24,10 +24,10 @@
         </tr>
         </thead>
         <tbody>
-        @foreach(range(1, 10) as $id)
+        @foreach($posts as $post)
             <tr>
-                <th scope="row" style="width: 50px">{{ $id }}</th>
-                <td>{{ '標題' . $id }}</td>
+                <th scope="row" style="width: 50px">{{ $post->id }}</th>
+                <td>{{ $post->title }}</td>
                 <td style="width: 150px">
                     <button type="button" class="btn btn-primary btn-sm">編輯</button>
                     <button type="button" class="btn btn-danger btn-sm">刪除</button>
