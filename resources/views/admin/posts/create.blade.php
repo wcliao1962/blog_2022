47eb0d2dd25a26d5ee4f4033b42b055f68274bc7
廖文忠 <wcliao1962@gmail.com>
2022-12-17 17:04:17 +0800

    資料驗證錯誤訊息呈現

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -9,11 +9,20 @@
         <li class="breadcrumb-item active">新增文章</li>
     </ol>
     <div class="alert alert-danger alert-dismissible" role="alert" id="liveAlert">
-        <strong>錯誤！</strong> 請修正以下問題：
-        <ul>
-            <li>錯誤 1</li>
-        </ul>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        @if (count($errors) > 0)
+            <!-- 表單錯誤清單 -->
+                <strong>哎呀！出了些問題！</strong>
+
+                <br><br>
+
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+
+        @endif
     </div>
     <form action="{{ route('admin.posts.store') }}" method="post">
         @method('POST')
