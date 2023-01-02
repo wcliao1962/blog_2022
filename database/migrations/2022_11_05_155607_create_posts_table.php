2f87e545250ea796b593867815a131c037057802
廖文忠 <wcliao1962@gmail.com>
2023-01-02 17:39:05 +0800

    posts新增FK (user_id)

--- database/migrations/2022_11_05_155607_create_posts_table.php
+++ database/migrations/2022_11_05_155607_create_posts_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
 
             $table->string('title');
             $table->text('content');
+            $table->unsignedBigInteger('user_id');
 
             $table->timestamps();
         });
