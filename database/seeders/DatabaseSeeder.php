c9cc350a8f3f4919ecdcabb475b051ed4402987d
廖文忠 <wcliao1962@gmail.com>
2023-01-02 17:46:10 +0800

    改呼叫UserSeeder產生資料

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PostsTableSeeder::class);
+//        $this->call(PostsTableSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
