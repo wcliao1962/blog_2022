580d2e95a0aa040ca5ff2d87d1fa5020bb4610d1
wcliao1962 <wcliao1962@gmail.com>
2023-01-03 17:02:29 +0800

    修改根路由(/)進入首頁後列出所有bloggers(使用者)的連結

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.index');
+        $bloggers=User::all();
+
+        $data = [
+            'bloggers' => $bloggers,
+        ];
+       //dd($bloggers);
+        return view('home.index', $data);
     }
 }
