3c4f892ab362199ca21a4058b9032b5c668fd251
wcliao1962 <wcliao1962@gmail.com>
2023-01-03 17:13:28 +0800

    修改進入個別blogger頁面為中文標題

--- resources/views/posts/shared/header.blade.php
+++ resources/views/posts/shared/header.blade.php
@@ -2,7 +2,7 @@
 <header class="py-5 bg-light border-bottom mb-4">
     <div class="container">
         <div class="text-center my-5">
-            <h1 class="fw-bolder">Welcome to <a href="{{ route('posts.index', $blogger->id) }}">{{$blogger->name}}</a>'s Home!</h1>
+            <h1 class="fw-bolder">歡迎來到<a href="{{ route('posts.index', $blogger->id) }}">{{$blogger->name}}</a>的城邦!</h1>
         </div>
     </div>
 </header>
